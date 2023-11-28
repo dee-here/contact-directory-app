214f7759c9b7fc0276f5bb40c47c97aa3f1a772a
Dee <deepakdilse@gmail.com>
2023-11-28 12:00:02 -0800

    Make the app a PWA

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,24 +2,55 @@
 import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
-
-};
+const initdb = async () =>
+  openDB('contacts', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('contacts')) {
+        console.log('contacts database already exists');
+        return;
+      }
+      db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+      console.log('contacts database created');
+    },
+  });
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+ const contactsDb = await openDB('contacts', 1);
+ const tx = contactsDb.transaction('contacts', 'readwrite');
+ const store = tx.objectStore('contacts');
+
+ const request = store.add({name: name, home_phone: home, cell_phone: cell, email: email  });
+
+ const result = await request;
+ console.log('Data saved to the database', result);
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readonly');
+    const store = tx.objectStore('contacts');
+
+    const request = store.getAll();
+
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts');
+
+    const request = store.delete(id);
+    const result = await request;
+
+    console.log('result.value', result);
+    return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
 
 module.exports = () => {
   return {
@@ -11,17 +15,57 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),   
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contacts List'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Contacts List App',
+        short_name: 'Contact List',
+        description: 'Save your contacts',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
   };
 };
